354cce824d8400009c3581927c7a16aa6a33c97c
senxzo <114511444+senxzo@users.noreply.github.com>
2024-07-30 18:05:25 -0400

    handle_print.c bug fixed

--- handle_print.c
+++ handle_print.c
@@ -12,10 +12,13 @@
  * @size: Size specifier
  * Return: 1 or 2;
  */
+
 int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	int flags, int width, int precision, int size)
 {
 	int i, unknow_len = 0, printed_chars = -1;
+
+	/* type specifier */
 	fmt_t fmt_types[] = {
 		{'c', print_char}, {'s', print_string}, {'%', print_percent},
 		{'i', print_int}, {'d', print_int}, {'b', print_binary},
@@ -23,6 +26,8 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 		{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
 		{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
 	};
+	/* get lenghth */
+	
 	for (i = 0; fmt_types[i].fmt != '\0'; i++)
 		if (fmt[*ind] == fmt_types[i].fmt)
 			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
