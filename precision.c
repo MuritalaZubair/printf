db14aada9a66aee8697021d1fee02bf179870d81
senxzo <114511444+senxzo@users.noreply.github.com>
2024-07-31 17:41:38 -0500

    Update precision.c

--- precision.c
+++ precision.c
@@ -3,46 +3,39 @@
 /**
  * get_precision - Calculates the precision for printing
  * @format: Formatted string in which to print the arguments
- * @i: Pointer to the current index in the format string
- * @list: List of arguments
+ * @i: List of arguments to be printed.
+ * @list: list of arguments.
  *
- * This function calculates the precision for printing based on the format
- * string and the list of arguments. The precision is used to determine
- * the number of digits to be printed for numeric values or the maximum
- * number of characters to be printed for strings.
- *
- * Return: The calculated precision. If no precision is specified, it
- * returns -1.
+ * Return: Precision.
  */
 int get_precision(const char *format, int *i, va_list list)
 {
-    int curr_i = *i + 1; /* Initialize current index */
-    int precision = -1;  /* Default precision value */
+	int curr_i = *i + 1; /* set the pointer */
+	int precision = -1;
 
-    if (format[curr_i] != '.')
-        return (precision); /* Return default if no precision specifier */
+	if (format[curr_i] != '.')
+		return (precision);
 
-    precision = 0; /* Initialize precision to 0 */
+	precision = 0;
 
-    /* Iterate through format string to calculate precision */
-    for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
-    {
-        if (is_digit(format[curr_i])) /* Check if character is a digit */
-        {
-            precision *= 10;
-            precision += format[curr_i] - '0'; /* Convert char to int */
-        }
-        else if (format[curr_i] == '*') /* Check for '*' specifier */
-        {
-            curr_i++;
-            precision = va_arg(list, int); /* Get precision from arguments */
-            break;
-        }
-        else
-            break; /* Break if no more precision specifier */
-    }
+	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
+	{
+		if (is_digit(format[curr_i]))
+		{
+			precision *= 10;
+			precision += format[curr_i] - '0';
+		}
+		else if (format[curr_i] == '*')
+		{
+			curr_i++;
+			precision = va_arg(list, int);
+			break;
+		}
+		else
+			break;
+	}
 
-    *i = curr_i - 1; /* Update index pointer */
+	*i = curr_i - 1;
 
-    return (precision); /* Return calculated precision */
+	return (precision);
 }
